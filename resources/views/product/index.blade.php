460e4bbcb5c2fa401ac1b94a35cb2b43d7e18930
Dmitriy Sirenko <dmitriy.sirenko@gmail.com>
2022-03-18 00:48:35 +0200

    buttons style

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -10,6 +10,7 @@
                 <th scope="col">Price</th>
                 <th scope="col">Description</th>
                 <th scope="col">&nbsp;</th>
+                <th scope="col">&nbsp;</th>
             </tr>
             </thead>
             <tbody>
@@ -19,8 +20,10 @@
                         <td><a href="{{route('products.show', $product->id)}}">{{ $product->name }}</a></td>
                         <td>{{ $product->price }}</td>
                         <td>{{ $product->description }}</td>
-                        <td>
+                        <td width="80">
                             <a href="{{ route('products.edit', $product->id) }}" class="btn btn-default">Edit</a>
+                        </td>
+                        <td width="80">
                             <form method="POST" id="delete-form-{{$product->id}}" action="{{route('products.destroy', [$product->id])}}">
                                 {{csrf_field()}}
                                 <input type="submit" name="destroy" value="Delete" class="btn btn-danger">
