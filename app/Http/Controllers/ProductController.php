cf1dddd00e172301612576fe4d6c16e27d74ef5b
Dmitriy Sirenko <dmitriy.sirenko@gmail.com>
2022-03-22 14:05:42 +0200

    latest products order in list

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return view('product.index', ['products' => Auth::user()->products()->paginate(10)]);
+        return view('product.index', ['products' => Auth::user()->products()->latest()->paginate(10)]);
     }
 
     /**
